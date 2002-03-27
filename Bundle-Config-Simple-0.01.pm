0b2075f3e4016e1514d14b57b759b9e41b1c0491
Sherzod Ruzmetov <sherzodr@cpan.org>
2002-03-27 14:24:42 +0000

    Import of SHERZODR/Bundle-Config-Simple-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Config-Simple
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHERZODR/Bundle-Config-Simple-0.01.tar.gz
    gitpan-cpan-author:       SHERZODR
    gitpan-cpan-maturity:     released

---
+++ Bundle-Config-Simple-0.01.pm
@@ -0,0 +1,58 @@
+package Bundle::Config::Simple;
+
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Config::Simple - Snapshot of installation on daffy on Wed Mar 27 07:17:43 2002
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Bundle::Config::Simple'
+
+=head1 CONTENTS
+
+Config::Simple 2.4
+
+Digest::MD5 2.16
+
+
+=head1 CONFIGURATION
+
+Summary of my perl5 (revision 5.0 version 6 subversion 1) configuration:
+  Platform:
+    osname=linux, osvers=2.4.17-0.13smp, archname=i386-linux
+    uname='linux daffy.perf.redhat.com 2.4.17-0.13smp #1 smp fri feb 1 10:30:48 est 2002 i686 unknown '
+    config_args='-des -Doptimize=-O2 -march=i386 -mcpu=i686 -Dcc=gcc -Dcf_by=Red Hat, Inc. -Dcccdlflags=-fPIC -Dinstallprefix=/usr -Dprefix=/usr -Darchname=i386-linux -Uusethreads -Uuseithreads -Uuselargefiles -Dd_dosuid -Dd_semctl_semun -Di_db -Di_ndbm -Di_gdbm -Di_shadow -Di_syslog -Dman3ext=3pm -Dlocincpth='
+    hint=recommended, useposix=true, d_sigaction=define
+    usethreads=undef use5005threads=undef useithreads=undef usemultiplicity=undef
+    useperlio=undef d_sfio=undef uselargefiles=undef usesocks=undef
+    use64bitint=undef use64bitall=undef uselongdouble=undef
+  Compiler:
+    cc='gcc', ccflags ='-fno-strict-aliasing -I/usr/local/include',
+    optimize='-O2 -march=i386 -mcpu=i686',
+    cppflags='-fno-strict-aliasing -I/usr/local/include'
+    ccversion='', gccversion='2.96 20000731 (Red Hat Linux 7.1 2.96-98)', gccosandvers=''
+    intsize=4, longsize=4, ptrsize=4, doublesize=8, byteorder=1234
+    d_longlong=define, longlongsize=8, d_longdbl=define, longdblsize=12
+    ivtype='long', ivsize=4, nvtype='double', nvsize=8, Off_t='off_t', lseeksize=4
+    alignbytes=4, usemymalloc=n, prototype=define
+  Linker and Libraries:
+    ld='gcc', ldflags =' -L/usr/local/lib'
+    libpth=/usr/local/lib /lib /usr/lib
+    libs=-lnsl -ldl -lm -lc -lcrypt -lutil
+    perllibs=-lnsl -ldl -lm -lc -lcrypt -lutil
+    libc=/lib/libc-2.2.4.so, so=so, useshrplib=false, libperl=libperl.a
+  Dynamic Linking:
+    dlsrc=dl_dlopen.xs, dlext=so, d_dlsymun=undef, ccdlflags='-rdynamic'
+    cccdlflags='-fPIC', lddlflags='-shared -L/usr/local/lib'
+
+
+
+=head1 AUTHOR
+
+This Bundle has been generated automatically by the autobundle routine in CPAN.pm.
